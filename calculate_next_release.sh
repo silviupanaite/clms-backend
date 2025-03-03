2a9b5c3b563529f827564f8f78f4dd0eda2b5a41
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2025-03-03 16:26:49 +0200

    [JENKINS] use latest version of calculate script

--- calculate_next_release.sh
+++ calculate_next_release.sh
@@ -1,1 +1,1 @@
-major=$(grep 'FROM .*:.* as base' Dockerfile |  grep -Eo '[0-9]+\.[0-9]+\.[0-9]+');let minor=$(git describe --tags --abbrev=0 2>/dev/null 2>/dev/null | grep -E $major-[0-9]+ | cut -d- -f2 )+1; echo $major-$minor
+major=$(grep FROM Dockerfile | awk -F':' '{print $2}' | awk -F'-' '{print $1}');let minor=$(git tag | grep -E $major-[0-9]+ | cut -d- -f2 | sort -n | tail -n 1 )+1; echo $major-$minor
